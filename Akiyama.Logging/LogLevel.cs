404d3623c84a812978186d138d0e6ac7514ac05d
iPeer <chrisdwignall+github@gmail.com>
2023-12-08 21:32:56 +0000

    Refactor: Deprecate short-name variants of `LogLevel`s

--- Akiyama.Logging/LogLevel.cs
+++ Akiyama.Logging/LogLevel.cs
@@ -1,15 +1,21 @@
-﻿namespace Akiyama.Logging
+﻿using System;
+
+namespace Akiyama.Logging
 {
     public enum LogLevel
     {
         DEBUG = 0,
-        DBG = 0,
         INFO = 1,
-        INF = 1,
         WARNING = 2,
         WARN = 2,
-        WRN = 2,
         ERROR = 3,
-        ERR = 3
+        [Obsolete("Short-name variants of logging levels are deprecated, use LogLevel.DEBUG instead")]
+        DBG = 0,
+        [Obsolete("Short-name variants of logging levels are deprecated, use LogLevel.INFO instead")]
+        INF = 1,
+        [Obsolete("Short-name variants of logging levels are deprecated, use LogLevel.WARNING instead")]
+        WRN = 2,
+        [Obsolete("Short-name variants of logging levels are deprecated, use LogLevel.ERROR instead")]
+        ERR = 3,
     }
 }
